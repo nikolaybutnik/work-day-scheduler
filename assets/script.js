fc6dcd1194cadba221ae4dd4faf5258e5d5f1b6e
mochi-dot-py <65631838+mochi-dot-py@users.noreply.github.com>
2020-09-15 22:18:18 -0400

    Refactored the code with more jQuery

--- assets/script.js
+++ assets/script.js
@@ -10,15 +10,16 @@ setInterval(() => {
 // Define a function that creates a time block for each hour of the work day
 function createRows() {
   let currentHourCompare = moment().format("HH:00");
-  let hourToInsert = "";
   for (let i = 8; i < 17; i++) {
     let textArea = $("<textarea>");
-    hourToInsert = moment().hour(i).format("HH:00");
-    textArea.attr("id", JSON.parse(JSON.stringify(hourToInsert)));
+    let hourToInsert = moment().hour(i).format("HH:00");
+    // Dynamically assign an id to each created textarea, to be used when writing/reading data from local storage
+    textArea.attr("id", `hour-${i}`);
     let timeBlock = $("<div>").addClass("row");
     let timeBlockHour = $("<div>").addClass("col-1 hour");
     timeBlockHour.text(hourToInsert);
     let timeBlockContent = $("<div>").addClass("col-10");
+    // Compare current hour to hour on the time block to assign a proper past/present/future class
     if (currentHourCompare === hourToInsert) {
       timeBlockContent.addClass("present");
       textArea.addClass("present");
@@ -34,8 +35,6 @@ function createRows() {
     timeBlockSave.append(createSaveButton());
     timeBlock.append(timeBlockHour, timeBlockContent, timeBlockSave);
     $(".container").append(timeBlock);
-    console.log(currentHourCompare);
-    console.log(hourToInsert);
   }
 }
 createRows();
@@ -65,15 +64,14 @@ $(".saveBtn").on("click", function (event) {
 
 // Define a function that loads data from local storage if it's present
 function getData() {
-  document.getElementById("08:00").textContent = localStorage.getItem("08:00");
-  document.getElementById("09:00").textContent = localStorage.getItem("09:00");
-  document.getElementById("10:00").textContent = localStorage.getItem("10:00");
-  document.getElementById("11:00").textContent = localStorage.getItem("11:00");
-  document.getElementById("12:00").textContent = localStorage.getItem("12:00");
-  document.getElementById("13:00").textContent = localStorage.getItem("13:00");
-  document.getElementById("14:00").textContent = localStorage.getItem("14:00");
-  document.getElementById("14:00").textContent = localStorage.getItem("14:00");
-  document.getElementById("15:00").textContent = localStorage.getItem("15:00");
-  document.getElementById("16:00").textContent = localStorage.getItem("16:00");
+  $("#hour-8").text(localStorage.getItem("hour-8"));
+  $("#hour-9").text(localStorage.getItem("hour-9"));
+  $("#hour-10").text(localStorage.getItem("hour-10"));
+  $("#hour-11").text(localStorage.getItem("hour-11"));
+  $("#hour-12").text(localStorage.getItem("hour-12"));
+  $("#hour-13").text(localStorage.getItem("hour-13"));
+  $("#hour-14").text(localStorage.getItem("hour-14"));
+  $("#hour-15").text(localStorage.getItem("hour-15"));
+  $("#hour-16").text(localStorage.getItem("hour-16"));
 }
 getData();
